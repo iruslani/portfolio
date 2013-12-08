694086bbf3012d919d3001ba9d1523188a56ca8c
iruslani <iruslani75@gmail.com>
2013-12-07 23:37:04 -0800

    adding p=heroku path for template files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,10 +31,20 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'template').replace('\\','/'),
     '/users/iwan/documents/websites/iwanruslani.com/template',
-
+    root('templates'),
 )
 
 # Quick-start development settings - unsuitable for production
