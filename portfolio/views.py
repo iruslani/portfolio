9c5c0c846eea8cb93895794f76c1d1603c0aaed0
iruslani <iruslani75@gmail.com>
2013-12-08 20:58:14 -0800

    added code for chapter 4 - template includes and template base

--- portfolio/views.py
+++ portfolio/views.py
@@ -11,14 +11,15 @@ def hello(request):
 
 def template(request):
     message = "Hello World"
-    t = get_template('base.html')
+    t = get_template('page.html')
     html = t.render(Context({'message': message}))
     return HttpResponse(html)
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('base.html')
-    html = t.render(Context({'message': now}))
+    t = get_template('page.html')
+    current_section = "Hours Ahead"
+    html = t.render(Context({'message': now , "current_section": current_section} ))
     return HttpResponse(html)
 
 def hours_ahead(request, offset):
@@ -27,7 +28,8 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    t = get_template('base.html')
+    t = get_template('page.html')
     message = "In %s hour(s), it will be %s." % (offset, dt)
-    html = t.render(Context({'message': message}))    
+    current_section = "Hours Ahead"
+    html = t.render(Context({'message': message, "current_section": current_section}))    
     return HttpResponse(html)
